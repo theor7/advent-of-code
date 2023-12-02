3310ef14b6ef04a3ccaa18a49648fbe4f873cec2
Theo <theo@yellowsubgeo.com>
2023-12-02 14:41:20 +0000

    day 1 complete

--- day1/day1.py
+++ day1/day1.py
@@ -8,11 +8,38 @@ to form a single two-digit number.
 
 import re
 
-line = 'nkzjrdqrmpztpqninetwofour1znnkd'
-
+# Part 1
 total = 0
-with open('day1.txt') as file:
+with open("day1.txt") as file:
     for line in file.readlines():
-        numbers = re.findall(r'\d', line)
+        numbers = re.findall(r"\d", line)
         total += int(numbers[0] + numbers[-1])
-print (total)
+
+print(f"Part 1 calibration value = {total}")
+
+# Part 2
+total = 0
+number_map = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+with open("day1.txt") as file:
+    for line in file.readlines():
+        # use lookahead to allow for overlaps
+        numbers = re.findall(
+            r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line
+        )
+        for i, number in enumerate([numbers[0], numbers[-1]]):
+            if not number.isnumeric():
+                numbers[i * -1] = number_map[number]
+        total += int(numbers[0] + numbers[-1])
+
+print(f"Part 2 calibration value = {total}")
